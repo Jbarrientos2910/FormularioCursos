81dd1ee08133aa7ca28ff83bbe464a6a0f79c932
Jbarrientos2910 <167161451+Jbarrientos2910@users.noreply.github.com>
2024-04-26 21:44:28 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace FormularioCursos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCurso1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCurso2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCurso3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Silver;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(655, 68);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnCurso1
+            // 
+            this.btnCurso1.Location = new System.Drawing.Point(47, 189);
+            this.btnCurso1.Name = "btnCurso1";
+            this.btnCurso1.Size = new System.Drawing.Size(165, 30);
+            this.btnCurso1.TabIndex = 1;
+            this.btnCurso1.Text = "Detalles del curso";
+            this.btnCurso1.UseVisualStyleBackColor = true;
+            this.btnCurso1.Click += new System.EventHandler(this.btnCurso1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(573, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CURSOS ACADÉMICOS \'IMAGINE KNOW SOMETHING\'";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(578, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "En este portal podrá visualizar todos los cursos disponibles para inscripción";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(428, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "1.- Curso de dominio de lenguaje programación Python.";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(374, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "2.- Curso de manejo y gestión de base de datos.";
+            // 
+            // btnCurso2
+            // 
+            this.btnCurso2.Location = new System.Drawing.Point(47, 279);
+            this.btnCurso2.Name = "btnCurso2";
+            this.btnCurso2.Size = new System.Drawing.Size(165, 30);
+            this.btnCurso2.TabIndex = 8;
+            this.btnCurso2.Text = "Detalles del curso";
+            this.btnCurso2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 344);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(426, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "3.- Curso de conocimientos generales sobre Hardware.";
+            // 
+            // btnCurso3
+            // 
+            this.btnCurso3.Location = new System.Drawing.Point(47, 365);
+            this.btnCurso3.Name = "btnCurso3";
+            this.btnCurso3.Size = new System.Drawing.Size(165, 30);
+            this.btnCurso3.TabIndex = 10;
+            this.btnCurso3.Text = "Detalles del curso";
+            this.btnCurso3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(691, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 31);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Ingresar nuevo curso";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(861, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 31);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Editar curso";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(1050, 596);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCurso3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnCurso2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCurso1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCurso1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCurso2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCurso3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularioCursos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCurso1_Click(object sender, EventArgs e)
+        {
+            Form p2 = new Form2();
+            p2.Show();
+        }
+    }
+}
